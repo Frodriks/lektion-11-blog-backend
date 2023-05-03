f5ffea0bfb26835d56c4b33a93801901c4921200
Frodriks <frodriks@gmail.com>
2023-05-03 15:25:55 +0200

    andra commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Standard stuff for backend
+const dbDriver = require('better-sqlite3');
+
+const db = dbDriver('blog.sqlite3');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('frontend'));
+
+/* Setup all routes */
+// GET
+app.get('/api/posts', (req, res) => {
+    const statement = db.prepare('SELECT * FROM posts');
+    const posts = statement.all();
+    res.json(posts);
+});
+
+// Get 1
+app.get('/api/posts/:id', (req, res) => {
+    const statement = db.prepare('SELECT * FROM posts WHERE id = ?');
+    const post = statement.get(req.params.id);
+    res.json(post);
+});
+
+// POST 
+app.post('/api/posts', (req, res) => {
+    const post = req.body;
+    const statement = db.prepare('INSERT INTO posts (title, content) VALUES (?, ?)');
+    const result = statement.run(post.title, post.content);
+    res.json({ id: result.lastInsertRowid, success: true});
+});
+
+/* Start server */
+app.listen(3000, console.log('Server starter on port 3000'));
